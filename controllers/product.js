938a44d11ac431c503b51d79045962866eaaf4ab
xuan.tranthithanh <xuan.tranthithanh@thalesdigital.io>
2021-05-09 14:15:22 +0200

    update product's details

--- controllers/product.js
+++ controllers/product.js
@@ -1,21 +1,46 @@
-
 const slugify = require("slugify");
 const Product = require("../models/Product");
 
 exports.create = (req, res) => {
-    req.body.slug = slugify(req.body.title)
-    Product.create(req.body)
-        .then(product => res.status(200).json(product))
-        .catch(err => {
-            console.log(err.message)
-            res.status(400).json({ message: err.message })
-        })
-}
+  req.body.slug = slugify(req.body.title);
+  Product.create(req.body)
+    .then((product) => res.status(200).json(product))
+    .catch((err) => {
+      console.log(err.message);
+      res.status(400).json({ message: err.message });
+    });
+};
 
 exports.readAllProducts = (req, res) => {
-    Product.find({})
-        .populate("Sub")
-        .then(prod => res.status(200).json(prod))
-        .catch(error => res.status(400).json({ message: error.message }))
+  Product.find({})
+    .limit(parseInt(req.params.count))
+    .populate("subs")
+    .populate("category")
+    .sort([["createdAt", "desc"]])
+    .then((prod) => res.status(200).json(prod))
+    .catch((error) => res.status(400).json({ message: error.message }));
+};
 
-}
+exports.readOneProduct = (req, res) => {
+  Product.findOne({ slug: req.params.slug })
+    .limit(parseInt(req.params.count))
+    .populate("subs")
+    .populate("category")
+    .then((prod) => res.status(200).json(prod))
+    .catch((error) => res.status(400).json({ message: error.message }));
+};
+
+exports.remove = (req, res) => {
+  Product.findOneAndDelete({ slug: req.params.slug })
+    .then((response) => res.status(200).json(response))
+    .catch((error) => res.status(400).json({ message: error.message }));
+};
+
+exports.update = (req, res) => {
+  if (req.body.title) {
+    req.body.slug = slugify(req.body.title);
+  }
+  Product.findOneAndUpdate({ slug: req.params.slug }, req.body, { new: true })
+    .then((product) => res.status(200).json(product))
+    .catch((err) => res.status(400).json({ message: err.message }));
+};
--- routes/product.js
+++ routes/product.js
@@ -1,8 +1,18 @@
 const express = require("express");
 const { authCheck, adminCheck } = require("../middlewares/auth");
-const { create, readAllProducts } = require("../controllers/product");
+const {
+  create,
+  readAllProducts,
+  remove,
+  readOneProduct,
+  update,
+} = require("../controllers/product");
 const router = express.Router();
 
-router.post("/product", authCheck, adminCheck, create)
-router.get("/product", readAllProducts)
+router.post("/product", authCheck, adminCheck, create);
+router.get("/products/:count", readAllProducts);
+router.delete("/product/:slug", authCheck, adminCheck, remove);
+router.get("/product/:slug", readOneProduct);
+router.put("/product/:slug", authCheck, adminCheck, update);
+
 module.exports = router;
