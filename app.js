b0065937a54ac5524afd9cad89d6a3cfc4005278
xuan.tranthithanh <xuan.tranthithanh@thalesdigital.io>
2021-06-28 00:47:12 +0200

    deploy

--- app.js
+++ app.js
@@ -21,6 +21,14 @@ readdirSync("./routes").map((route) =>
     app.use("/api", require("./routes/"+route))
 )
 
+//deploy on Heroku
+if (process.env.NODE_ENV === "production") {
+    app.use("*", (req, res, next) => {
+         // If no routes match, send them the React HTML.
+         res.sendFile(__dirname + "/public/index.html");
+    });
+}
+
 //port
 const port = process.env.PORT || 4000;
 app.listen(port, () => console.log(`Sever is running on port ${port}`))
